cee521121a01ba2f23bb25e5a73a6c5f3a99dbe9
David Denton <denton.david@gmail.com>
2022-05-08 19:32:28 +0100

    New action for reconfiguring a repository from a GHA pipeline

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# reconfigure
+# Monopolis: Reconfigure Repository
+
+This action is designed to work with Monopolis build workflows only. It reconfigures a repository from a GitHub Actions pipeline
+
+## Action inputs
+None. These are picked up automatically from the input event which triggered the workflow.
+
+## Action outputs
+None.
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/ash -xe
+
+REPO_PATH=$(jq -r '.repository.full_name' $GITHUB_EVENT_PATH)
+REF_PATH=$(jq -r '.ref' $GITHUB_EVENT_PATH)
+BRANCH=${REF_PATH:11}
+
+MONOPOLIS_URL="https://github-api.monopolis.cloud/reconfigure/$REPO_PATH?branch=$BRANCH"
+
+cat .monopolis/config.yml | \
+  curl --fail -X POST --data-binary @- \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "${MONOPOLIS_URL}"
+
