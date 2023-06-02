a6011f0f9337e978578a75b15e44c1fbb529e5a9
albertlatacz <albert.latacz@gmail.com>
2023-06-02 22:17:10 +0100

    use correct api and token

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ REPO_PATH=$(jq -r '.repository.full_name' $GITHUB_EVENT_PATH)
 REF_PATH=$(jq -r '.ref' $GITHUB_EVENT_PATH)
 BRANCH=${REF_PATH:11}
 
-MONOPOLIS_URL="https://github-api.monopolis.cloud/reconfigure/$REPO_PATH?branch=$BRANCH"
+MONOPOLIS_URL="https://api.monopolis.cloud/v1/reconfigure/$REPO_PATH?branch=$BRANCH"
 
 echo "Validating configuration..."
 
-STATUSCODE=$(cat monopolis.yml | curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST --data-binary @- -H "Authorization: Bearer ${GITHUB_TOKEN}" "${MONOPOLIS_URL}")
+STATUSCODE=$(cat monopolis.yml | curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST --data-binary @- -H "Authorization: Bearer ${MONOPOLIS_TOKEN}" "${MONOPOLIS_URL}")
 
 if test $STATUSCODE -ne 200; then
 	echo "Validation failure so exiting"
